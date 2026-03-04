ca89cbbb25ca27e1524798dbf06ea444d70901a9
Arsenio <arsenionet@gmail.com>
2026-03-04 12:02:15 -0600

    Hola a todos

--- HolaMundo.java
+++ HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola UVEG");
+        System.out.println("Hola a todos");
     }
     
 }
